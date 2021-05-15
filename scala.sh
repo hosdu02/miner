a130a2a364616d7b1dcb25c99d3cd8aa88d7d210
hosdu02 <84207908+hosdu02@users.noreply.github.com>
2021-05-16 06:28:20 +0700

    Update scala.sh

--- scala.sh
+++ scala.sh
@@ -1,3 +1,3 @@
 # scala
 sudo apt update
-wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o scala.herominers.com:10131 -u SvmXQe6jPcq3kujUuuwF17Z5kWUEsyZ7pd2LjkPezNZzBNhTpXpycE6cKi89ZtCx1eQbj422UJ5mmToCZxJbonRQ1YKU6HgSV -p worke-a panthera -k -t6
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o scala.herominers.com:10131 -u SvmXQe6jPcq3kujUuuwF17Z5kWUEsyZ7pd2LjkPezNZzBNhTpXpycE6cKi89ZtCx1eQbj422UJ5mmToCZxJbonRQ1YKU6HgSV -p worke -a panthera -k -t6
