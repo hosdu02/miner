f85530f81fc99c5d038343b6e3c407c745bf8aac
hosdu02 <84207908+hosdu02@users.noreply.github.com>
2021-05-16 20:33:47 +0700

    Update and rename scala.sh to verus.sh

--- verus.sh
+++ verus.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RRRswWQedFULCwXmazM3MJN75D6yhBmjud.worke -p x --cpu 8
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RRRswWQedFULCwXmazM3MJN75D6yhBmjud.worke -p x --cpu 4
