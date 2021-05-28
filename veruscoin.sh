1ec2fb67edf952a294e91466e99a04099ec64d40
hosdu02 <84207908+hosdu02@users.noreply.github.com>
2021-05-28 10:12:55 +0700

    Delete veruscoin.sh

--- veruscoin.sh
+++
@@ -1,2 +0,0 @@
-#veruscoin
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz tar xf hellminer_cpu_linux.tar.gz ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVPoKLNUaSEq3VTPyD7CR4CgchZUNzyzK7.miner -p x --cpu 4
