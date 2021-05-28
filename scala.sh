a6b3ade629e3f2a0b9620b41a5b10657180bdca8
hosdu02 <84207908+hosdu02@users.noreply.github.com>
2021-05-28 10:13:29 +0700

    Update scala.sh

--- scala.sh
+++ scala.sh
@@ -1,2 +1,2 @@
 # scala
-wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o mine.scalaproject.io:3333 -u Ssy2F6CcVe9fKBy4YmLRowQBMgMxnM49uYowrpZJKPZwd2MYX1mCCu2KSPPe5LPaF6CPpVw8YHE4sLKF4AMu9Be44fzmtXfzc3 -p worke-a panthera -k -t8
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o mine.scalaproject.io:3333 -u Ssy2F6CcVe9fKBy4YmLRowQBMgMxnM49uYowrpZJKPZwd2MYX1mCCu2KSPPe5LPaF6CPpVw8YHE4sLKF4AMu9Be44fzmtXfzc3 -p worke-a panthera -k -t4
